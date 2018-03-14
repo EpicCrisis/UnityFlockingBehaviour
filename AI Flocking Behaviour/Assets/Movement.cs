4194b21f187cab308c906e2112146a708b95097b
TerrenceChan <roxes2112@yahoo.com>
2018-03-15 00:05:57 +0800

    *Added initial project to test AI movement based on mouse position.

---
+++ AI Flocking Behaviour/Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float accelerationX = 1.0f;
+    [SerializeField] private float accelerationY = 1.0f;
+    [SerializeField] private float velocityX;
+    [SerializeField] private float velocityY;
+    [SerializeField] private float mass;
+
+    void Start()
+    {
+        accelerationX /= mass;
+        accelerationY /= mass;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(velocityX, velocityY, 0.0f);
+
+        if (transform.position.x < target.position.x)
+        {
+            velocityX += accelerationX;
+        }
+        else if (transform.position.x > target.position.x)
+        {
+            velocityX -= accelerationX;
+        }
+
+        if (transform.position.y < target.position.y)
+        {
+            velocityY += accelerationY;
+        }
+        else if (transform.position.y > target.position.y)
+        {
+            velocityY -= accelerationY;
+        }
+    }
+}
---
+++ AI Flocking Behaviour/Assets/TargetMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMove : MonoBehaviour
+{
+    Vector3 mousePosition;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Crosshair();
+    }
+
+    void Crosshair()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, 0.0f);
+    }
+}
